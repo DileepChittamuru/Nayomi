fed61060aae102dabf601bbbd0f3b41e6ca24ce2
DileepChittamuru <chdileep8500@gmail.com>
2018-02-05 15:15:13 +0530

    Add files via upload

--- sib-demo/src/app/dashboard/global-header/global-header.component.ts
+++ sib-demo/src/app/dashboard/global-header/global-header.component.ts
@@ -1,1 +1,27 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { ViewEncapsulation } from '@angular/core';
 
+@Component({
+  selector: 'app-global-header',
+  encapsulation: ViewEncapsulation.None,
+  templateUrl: './global-header.component.html',
+  styleUrls: ['./global-header.component.css']
+})
+export class GlobalHeaderComponent implements OnInit {
+  tab:string = '';
+  constructor() { }
+
+  ngOnInit() {
+  }
+  toggleTab(tabName,e) {
+	//e.stopPropogation();
+  //alert(tabName);
+    this.tab = tabName;
+    //this.title = this.translationLet[tabName];
+    // this.subtitle = this.translationLet
+  }
+  @Output() gotoOtion = new EventEmitter<string>();
+  gotoOtion(searchString){
+    this.preview.emit(searchString);
+  }
+}
